99f8bac6f019542710eecb5b4f56983c32f9bd66
Florian Sander <129592840+sanderiscoding@users.noreply.github.com>
2024-09-20 18:23:14 +0200

    inital commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function outputValues() {
+    const weightValue = document.getElementById("weight").value;
+    const creatinValue = document.getElementById("creatin").checked;
+
+    console.log(weightValue);
+    console.log(creatinValue);
+}
