08bc77ce3080bc333193666d9e738eefc8695247
Tim Rothenpieler <44950047+Tim-is-coding@users.noreply.github.com>
2024-09-25 18:29:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let clickCount = 0;
 document.getElementById('lottie_title').addEventListener('click', function() {
     clickCount++;
+    console.log(clickCount);
+    console.log(this);
     if (clickCount === 5) {
         this.load = 'https://lottie.host/208ea239-972f-4640-8813-f6fe7f20c71b/APy5NSK1iq.json';
         clickCount = 0; // Zähler zurücksetzen
